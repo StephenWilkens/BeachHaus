aaf196f6a7b29834f630cc76e4bd3cf0c13416e2
StephenWilkens <stephenwilkens@gmail.com>
2023-11-04 21:05:39 -0500

    Add comments and update

--- components/eventBookingForm.jsx
+++ components/eventBookingForm.jsx
@@ -1,11 +1,20 @@
 "use client";
 
-import { Button, Checkbox, Label, TextInput, Radio, Textarea, Datepicker } from "flowbite-react";
+import {
+  Button,
+  Checkbox,
+  Label,
+  TextInput,
+  Radio,
+  Textarea,
+  Datepicker,
+} from "flowbite-react";
 
 export function EventBookingForm() {
   return (
     <div class="py-5 px-5">
       <form className="flex max-w-md flex-col gap-4">
+        {/* First Name */}
         <div>
           <div className="mb-2 block">
             <Label htmlFor="firstName" value="First Name" />
@@ -17,6 +26,7 @@ export function EventBookingForm() {
             required
           />
         </div>
+        {/* Last Name */}
         <div>
           <div className="mb-2 block">
             <Label htmlFor="lastName" value="Last Name" />
@@ -28,6 +38,7 @@ export function EventBookingForm() {
             required
           />
         </div>
+        {/* Email */}
         <div>
           <div className="mb-2 block">
             <Label htmlFor="email1" value="Your email" />
@@ -39,6 +50,7 @@ export function EventBookingForm() {
             required
           />
         </div>
+        {/* Phone Number */}
         <div>
           <div className="mb-2 block">
             <Label htmlFor="phoneNumber" value="Phone Number" />
@@ -50,8 +62,9 @@ export function EventBookingForm() {
             required
           />
         </div>
+        What is your prefered contact method?
         <fieldset className="flex max-w-md flex-col gap-4">
-          <legend className="mb-4">What is your prefered contact method?</legend>
+          <legend className="mb-4">{/* Contact Pref*/}</legend>
           <div className="flex items-center gap-2">
             <Radio
               id="email"
@@ -59,23 +72,21 @@ export function EventBookingForm() {
               value="email"
               defaultChecked
             />
-            <Label htmlFor="united-state">Phone</Label>
+            <Label htmlFor="email">Email</Label>
           </div>
           <div className="flex items-center gap-2">
             <Radio id="phone" name="contactForms" value="phone" />
-            <Label htmlFor="germany">Phone</Label>
+            <Label htmlFor="phone">Phone</Label>
           </div>
         </fieldset>
+        {/* Company*/}
         <div>
           <div className="mb-2 block">
             <Label htmlFor="company" value="Company" />
           </div>
-          <TextInput
-            id="company"
-            type="text"
-            placeholder="Company Name"
-          />
+          <TextInput id="company" type="text" placeholder="Company Name" />
         </div>
+        {/* Event Description*/}
         <div>
           <div className="mb-2 block">
             <Label htmlFor="eventDescription" value="Event Description" />
@@ -87,12 +98,29 @@ export function EventBookingForm() {
             required
           />
         </div>
+        {/* Location ID (if multiple locations?)*/}
+        {/* Event Date*/}
         <div>
           <div class="mb-2 block">
             <Label htmlFor="eventDate" value="Event Date" />
           </div>
           <Datepicker id="eventDate" />
         </div>
+        {/* Start Time*/}
+        {/* End Time*/}
+        {/* Guest Count*/}
+        {/* Additional Info*/}
+        {/* lead form ID*/}
+        {/* email opt in*/}
+        {/* lead source id*/}
+        {/* referral source id*/}
+        {/* referral source other*/}
+        {/* gdpr_consent_granted - with the value 1 to indicate that the lead has consented to data collection as required by GDP*/}
+        {/* event_style - Accepted String values as follows:
+          onpremise (On-Premise)
+          pickup (Pickup Catering)
+          dropoff (Drop-Off Catering)
+          catering (Full-Service Catering)*/}
         <Button type="submit">Submit</Button>
       </form>
     </div>
