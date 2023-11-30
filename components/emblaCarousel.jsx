00f9bebaa1ec67b9d52bc51d2e99e5e1f62c24ab
StephenWilkens <stephenwilkens@gmail.com>
2023-11-30 17:31:31 -0600

    Updated carousel with links and pics

--- components/emblaCarousel.jsx
+++ components/emblaCarousel.jsx
@@ -6,6 +6,10 @@ import { useState, useEffect, useCallback } from "react";
 import Link from "next/link";
 import Autoplay from "embla-carousel-autoplay";
 import flights from "../public/bhFlights.png";
+import fullTable from "../public/801FullTable.jpg";
+import rooftopDrink from "../public/rooftopSunsetDrink.jpeg";
+import rooftopPour from "../public/rooftopPour.JPG";
+import craftCool from "../public/801Cool.png";
 //[Autoplay()]
 
 export const EmblaCarousel = () => {
@@ -13,39 +17,95 @@ export const EmblaCarousel = () => {
 
   return (
     <div className="embla overflow-hidden w-full" ref={emblaRef}>
-      <div className="embla__container flex lg:max-h-[540px] 2xl:max-h-[780px] items-center">
-        <div className="embla__slide bg-black relative flex flex-none flex-wrap lg:flex-nowrap w-full">
+      <div className="embla__container flex max-h-[360px] lg:max-h-[540px] 2xl:max-h-[780px] items-center">
+        <div className="embla__slide bg-black relative flex flex-none flex-wrap lg:flex-nowrap w-full min-h-[360px] lg:max-h-[540px] 2xl:max-h-[780px]">
           <Image
-            src={flights}
+            src={rooftopPour }
             alt="flight of beers"
-            class="opacity-70 w-full"
+            class="opacity-70 w-full object-cover"
           />
-          <div className="absolute inset-0 flex items-center justify-between">
-            <h1 className="text-white text-8xl">
+          <div className="absolute inset-0 flex flex-col items-center md:items-start justify-center md:gap-4">
+            <h1 className="text-white text-center md:text-start text-5xl font-bold md:text-8xl md:pl-3">
+              The Rooftop
+              <br />
+              at Beach Haus
+            </h1>
+            <p className="text-white text-center md:text-start pt-2 px-12 md:px-0 md:pl-3 text-xl md:text-2xl">
+              Delicious food, award winning beer, and fresh cocktails
+            </p>
+            <button class="bg-cyan-800 hover:bg-cyan-600 text-white py-2 px-2 mt-3 rounded ml-3">
+              <Link href="/brewery">Explore our Menu</Link>
+            </button>
+          </div>
+        </div>
+        <div className="embla__slide bg-black relative flex flex-none flex-wrap lg:flex-nowrap w-full min-h-[360px] lg:max-h-[540px] 2xl:max-h-[780px]">
+          <Image
+            src={fullTable }
+            alt="flight of beers"
+            class="opacity-70 w-full object-cover"
+          />
+          <div className="absolute inset-0 flex flex-col items-center md:items-start justify-center md:gap-4">
+            <h1 className="text-white text-center md:text-start text-5xl font-bold md:text-8xl md:pl-3">
               The Brewery
               <br />
               at Beach Haus
             </h1>
-            <p className="text-white text-2xl">Great food, fresh beer, and cocktails</p>
+            <p className="text-white text-center md:text-start pt-2 px-12 md:px-0 md:pl-3 text-xl md:text-2xl">
+              Delicious food, award winning beer, and fresh cocktails
+            </p>
+            <button class="bg-cyan-800 hover:bg-cyan-600 text-white py-2 px-2 mt-3 rounded ml-3">
+              <Link href="/brewery">Explore our Menu</Link>
+            </button>
           </div>
         </div>
-        <div className="embla__slide relative flex flex-none flex-wrap lg:flex-nowrap w-full mx-10">
-          Slide 2
+        <div className="embla__slide bg-black relative flex flex-none flex-wrap lg:flex-nowrap w-full min-h-[360px] lg:max-h-[540px] 2xl:max-h-[780px]">
+          <Image
+            src={craftCool }
+            alt="flight of beers"
+            class="opacity-70 w-full object-cover"
+          />
+          <div className="absolute inset-0 flex flex-col items-center justify-center md:gap-4">
+            <h1 className="text-white text-center md:text-start text-5xl font-bold md:text-8xl md:pl-3">
+              801 Craft Kitchen
+            </h1>
+            <p className="text-white text-center md:text-start pt-2 px-12 md:px-0 md:pl-3 text-xl md:text-2xl">
+              Fine dining at the shore
+            </p>
+            <button class="bg-cyan-800 hover:bg-cyan-600 text-white py-2 px-2 mt-3 rounded ml-3">
+              <Link href="/brewery">Explore our Menu</Link>
+            </button>
+          </div>
         </div>
-        <div className="embla__slide relative flex flex-none flex-wrap lg:flex-nowrap w-full mx-10">
-          Slide 3
+        <div className="embla__slide bg-black relative flex flex-none flex-wrap lg:flex-nowrap w-full min-h-[360px] lg:max-h-[540px] 2xl:max-h-[780px]">
+          <Image
+            src={ flights }
+            alt="flight"
+            class="opacity-70 w-full object-cover"
+          />
+          <div className="absolute inset-0 flex flex-col items-center md:items-end justify-center md:gap-4">
+            <h1 className="text-white text-center md:text-start text-5xl font-bold md:text-8xl md:pr-3">
+              Lobby Bar
+            </h1>
+            <p className="text-white text-center md:text-start pt-2 px-12 md:px-0 md:pr-3 text-xl md:text-2xl">
+              Lounge Area
+            </p>
+            <button class="bg-cyan-800 hover:bg-cyan-600 text-white py-2 px-2 mt-3 rounded mr-3">
+              <Link href="/brewery">See the space!</Link>
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-
-{/* <div className="absolute inset-0 flex items-center justify-between">
+{
+  /* <div className="absolute inset-0 flex items-center justify-between">
             <h1 className="text-white text-8xl">
               The Brewery
               <br />
               at Beach Haus
             </h1>
             <p className="text-white text-2xl">Great food, fresh beer, and cocktails</p>
-          </div> */}
+          </div> */
+}
