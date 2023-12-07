18fb1dcc3a6e0e19b104bc4ac051a1eabb05cd50
StephenWilkens <stephenwilkens@gmail.com>
2023-12-06 18:35:47 -0600

    add private events route

--- app/private-events/page.js
+++ app/private-events/page.js
@@ -1,12 +1,14 @@
 import { PrivateEventsHeader } from "@/components/privateEventsHeader";
 import { PrivateEventsAbout } from "@/components/privateEventsAbout";
 import { TripSeatScript } from "@/components/tripleSeatScript";
+import { VenueOptions } from "@/components/venueOptions";
 export default function PrivateEvents() {
   return (
     <div>
       <PrivateEventsHeader />
       <PrivateEventsAbout />
       <TripSeatScript />
+      <VenueOptions />
     </div>
   );
 }
