6e3dbad7dcfcc7a7153f0e91b6c24023537fcdb0
StephenWilkens <stephenwilkens@gmail.com>
2023-12-07 18:01:27 -0600

    add new components to venue options

--- components/venueOptions.jsx
+++ components/venueOptions.jsx
@@ -1,4 +1,9 @@
-import {BeerHallSemiPrivateCarousel} from "@/components/beerHallSemiPrivate"
+import { BeerHallSemiPrivateCarousel } from "@/components/beerHallSemiPrivate";
+import { BeerHallPrivateCarousel } from "./berrHallPrivateCarousel";
+import { CraftKitchenCarousel } from "./craftKitchenCarousel";
+import { LobbyCarousel } from "./lobbyCarousel";
+import { TapPatioBeerHallCarousel } from "./tapPatioBeerHallCarousel";
+
 export function VenueOptions() {
   return (
     <div className="pt-10">
@@ -15,6 +20,33 @@ export function VenueOptions() {
       </div>
       <h1 className="text-xl pl-16">•Semi-Private Capacity: 20-64 Guests</h1>
       <BeerHallSemiPrivateCarousel />
+      <h1 className="text-xl pl-16">•Private Capacity: 65-130 Guests</h1>
+      <BeerHallPrivateCarousel />
+      <div className="flex pl-10">
+        <h1 className="font-bold text-2xl">801 Craft Kitchen & Lounge</h1>
+        <p className="text-xl">(Private)</p>
+      </div>
+      <h1 className="text-xl pl-16">
+        •Seated Capacity: 20-65 Guests (including lounge area)
+      </h1>
+      <h1 className="text-xl pl-16">
+        •Standing Capacity: 20-100 Guests (including lounge area)
+      </h1>
+      <CraftKitchenCarousel />
+      <div className="flex pl-10">
+        <h1 className="font-bold text-2xl">Lobby Bar</h1>
+        <p className="text-xl">(Semi-Private)</p>
+      </div>
+      <h1 className="text-xl pl-16">•Capacity: 20-55 Guests</h1>
+      <LobbyCarousel />
+      <div className="flex pl-10">
+        <h1 className="font-bold text-2xl">
+          Tap Room, Patio, & Beer Hall Buyout
+        </h1>
+        <p className="text-xl">(Private)</p>
+      </div>
+      <h1 className="text-xl pl-16">•Capacity: 300 Guests</h1>
+      <TapPatioBeerHallCarousel />
     </div>
   );
 }
