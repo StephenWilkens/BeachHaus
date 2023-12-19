72f16325b741c8468d5fa3101e0de9f174058096
StephenWilkens <stephenwilkens@gmail.com>
2023-12-19 14:18:10 -0600

    add pages for booking form and venue options

---
+++ app/private-events/booking/page.js
@@ -0,0 +1,11 @@
+import { PrivateEventsHeader } from "@/components/privateEventsHeader";
+import { TripSeatScript } from "@/components/tripleSeatScript";
+
+export default function Booking() {
+  return (
+    <div>
+      <PrivateEventsHeader />
+      <TripSeatScript />
+    </div>
+  )
+}
---
+++ app/private-events/venue-options/page.js
@@ -0,0 +1,9 @@
+import { PrivateEventsVenueOptions } from "@/components/privateEventsVenueOptions";
+
+export default function VenueOptions() {
+  return (
+    <div>
+      <PrivateEventsVenueOptions />
+    </div>
+  )
+}
