33fbf2a4655b4059abc9910e6c8c1485d44fe6b7
StephenWilkens <stephenwilkens@gmail.com>
2023-12-21 19:38:44 -0600

    add craft kitchen venue page

---
+++ app/private-events/venue-options/craft-kitchen/page.js
@@ -0,0 +1,9 @@
+import { CraftKitchenVenue } from "@/components/craftKitchenVenue";
+
+export default function CraftKitchenVenuePage() {
+  return (
+    <div>
+      <CraftKitchenVenue />
+    </div>
+  )
+}
