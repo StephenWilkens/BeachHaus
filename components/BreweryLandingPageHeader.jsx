1825a22ef5c939dda47646800b150aaf0d59e7e5
StephenWilkens <stephenwilkens@gmail.com>
2024-02-02 14:49:24 -0500

    2xl screen size accomodations

--- components/BreweryLandingPageHeader.jsx
+++ components/BreweryLandingPageHeader.jsx
@@ -14,7 +14,7 @@ export function BreweryLandingPageHeader() {
           <div class="md:col-span-1 md:w-2/6 md:h-4/6 md:bg-black md:absolute md:opacity-70 md:inset-y-24 md:inset-x-28"></div>
         </div>
         <div class="z-10 col-span-2 h-full p-10 w-full text-center md:col-span-1 relative">
-          <h1 class="z-20 text-2xl text-white font-bold md:text-6xl md:py-36 md:px-40 md:text-left  ">
+          <h1 class="z-20 text-2xl text-white font-bold md:text-6xl md:py-36 md:px-40 md:text-left 2xl:text-8xl 2xl:mr-44 2xl:ml-12 2xl:py-52">
             The Brewery at Beach Haus
           </h1>
         </div>
