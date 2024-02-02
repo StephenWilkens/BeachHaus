37b1d4dae0a4afbb60d7cdcb899359f21fc5c5b2
StephenWilkens <stephenwilkens@gmail.com>
2024-02-02 18:35:31 -0500

    updated packages

--- components/breweryMain.jsx
+++ components/breweryMain.jsx
@@ -7,13 +7,18 @@ import Link from "next/link";
 import { BreweryLandingPageHeader } from "./BreweryLandingPageHeader";
 import { BreweryLandingPageFoodMenuContentRow } from "./BreweryLandingPageFoodMenuContentRow";
 import { BreweryLandingPageCocktailsContentRow } from "./BreweryLandingPageCocktailsContentRow";
+import { BreweryLandingPageBeerContentRow } from "./BreweryLandingPageBeerContentRow";
+import { BreweryLandingPageContractBrewContentRow } from "./BreweryLandingPageContracBrewContentRow";
+
 
 export function BreweryMain() {
   return (
-    <div className="bg-black md:bg-gray-100">
+    <div className="bg-black md:bg-cyan-50">
       <BreweryLandingPageHeader />
       <BreweryLandingPageFoodMenuContentRow />
       <BreweryLandingPageCocktailsContentRow />
+      <BreweryLandingPageBeerContentRow />
+      <BreweryLandingPageContractBrewContentRow />
     </div>
   );
 }
