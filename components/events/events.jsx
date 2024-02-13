8c43eb2e76c4bd53422575d414cc4b41adf2ad50
StephenWilkens <stephenwilkens@gmail.com>
2024-02-13 12:48:30 -0500

    create events calendar iframe component

---
+++ components/events/events.jsx
@@ -0,0 +1,7 @@
+export function Events() {
+  return(
+    <div className="w-full min-h-screen overflow-hidden">
+      <iframe src="https://beachhausparty.com/calendar/" className="w-full aspect-auto h-screen" allowfullscreen />
+    </div>
+  )
+}
