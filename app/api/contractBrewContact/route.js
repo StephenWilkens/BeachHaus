a73eb51e00dd0e493ca7899a6baecc6ca4fd6ad3
StephenWilkens <stephenwilkens@gmail.com>
2024-02-14 15:42:10 -0500

    promise within try catch

--- app/api/contractBrewContact/route.js
+++ app/api/contractBrewContact/route.js
@@ -25,29 +25,41 @@ export async function POST(request) {
     },
   });
 
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(
-      {
-        from: user,
-        to: user,
-        replyTo: data.email,
-        subject: `Contract Brewing Inquerey from ${name}`,
-        html: `
-    <p>Name: ${name}</p>
-    <p>Email: ${email}</p>
-    <p>Phone Number: ${phone}</p>
-    <p>Brew Information: ${beerInfo}</p>`,
-      },
-      (err, info) => {
-        if (err) {
-          console.log(err);
-          reject(err);
-        } else {
-          resolve(info);
-        }
-      },
+  try {
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(
+        {
+          from: user,
+          to: user,
+          replyTo: data.email,
+          subject: `Contract Brewing Inquerey from ${name}`,
+          html: `
+      <p>Name: ${name}</p>
+      <p>Email: ${email}</p>
+      <p>Phone Number: ${phone}</p>
+      <p>Brew Information: ${beerInfo}</p>`,
+        },
+        (err, info) => {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            resolve(info);
+          }
+        },
+      );
+    });
+    return NextResponse.json(
+      { message: "Email Sent Successfully" },
+      { status: 200 },
     );
-  });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Failed to Send Email" },
+      { status: 500 },
+    );
+  }
 }
 // try {
 //   const mail = await transporter.sendMail({
