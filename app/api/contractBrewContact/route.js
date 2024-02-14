00c3a5058e7fa2b258121f5cc881f392473fcc9f
StephenWilkens <stephenwilkens@gmail.com>
2024-02-14 15:26:57 -0500

    remove try catch for promise

--- app/api/contractBrewContact/route.js
+++ app/api/contractBrewContact/route.js
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
+import { resolve } from "styled-jsx/css";
 // import nodemailer from "nodemailer";
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 export async function POST(request) {
   const { name, email, phone, beerInfo } = await request.json();
-  
+
   const user = process.env.user;
 
   const data = {
@@ -24,28 +25,51 @@ export async function POST(request) {
     },
   });
 
-  try {
-    const mail = await transporter.sendMail({
-      from: user,
-      to: user,
-      replyTo: data.email,
-      subject: `Contract Brewing Inquerey from ${name}`,
-      html: `
-      <p>Name: ${name}</p>
-      <p>Email: ${email}</p>
-      <p>Phone Number: ${phone}</p>
-      <p>Brew Information: ${beerInfo}</p>`,
-    });
-
-    return NextResponse.json(
-      { message: "Email Sent Successfully" },
-      { status: 200 },
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(
+      {
+        from: user,
+        to: user,
+        replyTo: data.email,
+        subject: `Contract Brewing Inquerey from ${name}`,
+        html: `
+    <p>Name: ${name}</p>
+    <p>Email: ${email}</p>
+    <p>Phone Number: ${phone}</p>
+    <p>Brew Information: ${beerInfo}</p>`,
+      },
+      (err, info) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(info);
+        }
+      },
     );
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { message: "Failed to Send Email" },
-      { status: 500 },
-    );
-  }
+  });
 }
+// try {
+//   const mail = await transporter.sendMail({
+//     from: user,
+//     to: user,
+//     replyTo: data.email,
+//     subject: `Contract Brewing Inquerey from ${name}`,
+//     html: `
+//     <p>Name: ${name}</p>
+//     <p>Email: ${email}</p>
+//     <p>Phone Number: ${phone}</p>
+//     <p>Brew Information: ${beerInfo}</p>`,
+//   });
+
+//   return NextResponse.json(
+//     { message: "Email Sent Successfully" },
+//     { status: 200 },
+//   );
+// } catch (error) {
+//   console.log(error);
+//   return NextResponse.json(
+//     { message: "Failed to Send Email" },
+//     { status: 500 },
+//   );
+// }
