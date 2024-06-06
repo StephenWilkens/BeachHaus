340f2d7154be0973f7ed53167b4042515ccaaae4
StephenWilkens <stephenwilkens@gmail.com>
2024-06-06 19:49:57 -0400

    update carousel on landing

--- components/landingPage/LandingPageCarousel.jsx
+++ components/landingPage/LandingPageCarousel.jsx
@@ -4,16 +4,15 @@ import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
 import Link from "next/link";
 import Autoplay from "embla-carousel-autoplay";
-import flights from "../../public/bhFlights.png";
-import fullTable from "../../public/801FullTable.jpg";
-import rooftopPour from "../../public/rooftopPour.JPG";
-import craftCool from "../../public/801Cool.png";
+
 import trivia from "../../public/16x9Crop/BH_16X9trivia.jpg";
 import testDept from "../../public/16x9Crop/BH_16x9testDept.jpg";
-import contractBrewImg from "../../public/16x9Crop/BH_16x9contractBrewing.jpg";
 import distroImg from "../../public/16x9Crop/BH_16x9_DistroSlider.jpg";
 import burgersImg from "../../public/16x9Crop/BH_BurgerSlider.png";
 import liveMusicImg from "../../public/16x9Crop/BH_16x9_HausPartySlider.jpg";
+import musicBingo from "../../public/MusicBingo.jpg"
+import karaoke from "../../public/Karaoke.jpg"
+import cigarNight from "../../public/SUNSETS&CIGARS.jpg"
 //[Autoplay()]
 // stopOnInteraction: false
 export const LandingPageCarousel = () => {
@@ -23,33 +22,29 @@ export const LandingPageCarousel = () => {
 
   return (
     <div className="embla w-full overflow-hidden" ref={emblaRef}>
-      <div className="embla__container flex items-center lg:max-h-[720px] 2xl:max-h-[780px]">
-        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-[720px] lg:flex-nowrap 2xl:max-h-[780px]">
+      <div className="embla__container flex items-center lg:max-h-full 2xl:max-h-[780px]">
+        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
           <Image
-            src={burgersImg}
+            src={karaoke}
             alt="flight of beers"
-            class="w-full object-cover opacity-70"
+            class="w-full object-cover"
           />
-          <div className="absolute inset-0 flex flex-col items-center justify-center md:items-start md:gap-4">
-            <h1 className="text-center text-5xl font-bold text-white md:pl-3 md:text-start md:text-8xl">
-              Burger Mondays
-            </h1>
-            <p className="px-12 pt-2 text-center text-xl text-white md:px-0 md:pl-3 md:text-start md:text-2xl">
-              Every Monday, Buy 1 Burger, Get a 2nd Burger Half Off!
-            </p>
-            <button class="ml-3 mt-3 rounded bg-cyan-800 px-2 py-2 text-white hover:bg-cyan-600">
-              <Link href="/brewery/food">See the Burger Menu!</Link>
-            </button>
-          </div>
         </div>
-        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-[720px] lg:flex-nowrap 2xl:max-h-[780px]">
+        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
+          <Image
+            src={musicBingo}
+            alt="flight of beers"
+            class="w-full object-cover"
+          />
+        </div>
+        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
           <Image
             src={trivia}
             alt="flight of beers"
             class="w-full object-cover"
           />
         </div>
-        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-[720px] lg:flex-nowrap 2xl:max-h-[780px]">
+        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
           <Image
             src={testDept}
             alt="flight of beers"
@@ -68,14 +63,14 @@ export const LandingPageCarousel = () => {
             </p>
           </div>
         </div>
-        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-[720px] lg:flex-nowrap 2xl:max-h-[780px]">
+        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
             <Image
-              src={liveMusicImg}
+              src={cigarNight}
               alt="flight"
               class="w-full object-contain lg:object-cover"
             />
         </div>
-        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-[720px] lg:flex-nowrap 2xl:max-h-[780px]">
+        <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
           <Image
             src={distroImg}
             alt="flight"
