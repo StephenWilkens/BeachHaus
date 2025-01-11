1bf87354d4ebc0fbfed1f6505b7d9c384ad7fae9
StephenWilkens <stephenwilkens@gmail.com>
2025-01-11 15:51:40 -0500

    add link to chili cook off img

--- components/landingPage/LandingPageCarousel.jsx
+++ components/landingPage/LandingPageCarousel.jsx
@@ -34,35 +34,33 @@ export const LandingPageCarousel = () => {
     <div className="embla w-full overflow-hidden" ref={emblaRef}>
       <div className="embla__container flex items-center lg:max-h-full 2xl:max-h-[780px]">
         <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
-          <Image
-            src={slider1}
-            alt="flight of beers"
-                        class="w-full object-contain lg:object-cover"
-
-          />
+          <Link href="https://www.eventbrite.com/e/beach-haus-brewery-tickets-1134030789619?aff=oddtdtcreator">
+            <Image
+              src={slider1}
+              alt="flight of beers"
+              class="w-full object-contain lg:object-cover"
+            />
+          </Link>
         </div>
         <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
           <Image
             src={slider2}
             alt="flight of beers"
-                        class="w-full object-contain lg:object-cover"
-
+            class="w-full object-contain lg:object-cover"
           />
         </div>
         <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
           <Image
             src={slider3}
             alt="flight of beers"
-                        class="w-full object-contain lg:object-cover"
-
+            class="w-full object-contain lg:object-cover"
           />
         </div>
         <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
           <Image
             src={slider4}
             alt="flight of beers"
-                        class="w-full object-contain lg:object-cover"
-
+            class="w-full object-contain lg:object-cover"
           />
         </div>
         <div className="embla__slide relative flex min-h-[360px] w-full flex-none flex-wrap bg-black lg:max-h-full lg:flex-nowrap 2xl:max-h-[780px]">
