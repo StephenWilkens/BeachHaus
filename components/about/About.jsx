0b41b5568faef3c0f02f3451420844805720dabb
StephenWilkens <stephenwilkens@gmail.com>
2026-01-11 18:39:11 -0500

    hours

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -84,13 +84,19 @@ export function About() {
           </p>
           
           <p className="mx-5 pt-1">
-            <b>Monday - Thursday:</b> 4PM-10PM
+            <b>Monday - Tuesday:</b> 4PM-10PM
+          </p>
+          <p className="mx-5 pt-1">
+            <b>Wednesday:</b> Closed
+          </p>
+          <p className="mx-5 pt-1">
+            <b>Thursday</b> 4PM-10PM
           </p>
           <p className=" mx-5">
-            <b>Friday & Saturday:</b> 12PM-11AM
+            <b>Friday & Saturday:</b> 12PM-10PM
           </p>
           <p className=" mx-5">
-            <b>Sunday:</b> 12PM-9PM
+            <b>Sunday:</b> 12PM-8PM
           </p>
           <p className=" mx-5 pt-1">
             <b>Please note:</b> The kitchen shuts down one hour before closing time
